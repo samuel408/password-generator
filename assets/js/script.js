1700e5852211c5047e6dea82efa2b75aba09afa9
Samuel <samuel@Samuels-MacBook-Pro.local>
2020-12-11 10:51:32 -0800

    created  character length function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,31 @@
 // Assignment code here
-var x =[1, 2, 3,4 ]
+// character length function that propts user and returns length
+function characterLength(){
+  var input = window.prompt("How many Characters Long ? (8 Minimum  & 128 Maximum)");
+// out of range answer fix 
+  if (input < 8 || input >128){
+    window.alert(" ERROR Character length can only be from 8-128 characters long ");
+    characterLength();
+  }
+  else {
+    return input ;
+  }
+
+}
+
+/* create function that  prompts user to  select certain character types
+  and then stores them in an array using a loop.
+
+*/
 // create function for generatePassword()
+
 function generatePassword(){
-var turtle = 2;
-  return turtle;
+  // get character length from user  and use the value to demonstate how long it will be 
+var charLength = characterLength();
+return charLength;
+  // get character types from user and demonstrate the types shown
+
+  // finally generate password and return it 
 }
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");

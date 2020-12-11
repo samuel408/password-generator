e4289c8c8e3ff2f0a0305a127d17ccf4cf851340
Samuel <samuel@Samuels-MacBook-Pro.local>
2020-12-11 13:45:53 -0800

    created the char-type function to see how many types they want to use I also thought it would be better choose WHICH ones they want to use in the password generator function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,13 @@
 // Assignment code here
 // character length function that propts user and returns length
+
 function characterLength(){
   var input = window.prompt("How many Characters Long ? (8 Minimum  & 128 Maximum)");
 // out of range answer fix 
   if (input < 8 || input >128){
     window.alert(" ERROR Character length can only be from 8-128 characters long ");
     characterLength();
+
   }
   else {
     return input ;
@@ -17,16 +19,92 @@ function characterLength(){
   and then stores them in an array using a loop.
 
 */
+
+
+function characterType(){
+  
+  
+// function loop(){
+//   var types =[];
+//     for(var i = 0; i < count; i++){
+//        var input = window.prompt("What character types would you like to use ? (lowercase, uppercase, numeric, and/or special characters (SPELL EXACTLY AS THEY APPEAR INCLUDING THE SPACE IN SPECIAL CHARACTER)");
+//        if (input != "lowercase" && input != "uppercase" && input != "numeric" &&  input != "special character"){
+//         window.alert(" ERROR Character types can only be lowercase, uppercase, numeric, and/or special characters");
+//         loop();
+//       }
+//       else {
+//         types[i] = input;
+//       }
+      
+
+//     }
+
+//   }
+
+  var count = 0;
+  count  = window.prompt("how many character types would you like to use?  please choose between 1-4 types(lowercase, uppercase, numeric, and/or special characters )" );
+  if (count < 1 || count  > 4){
+    window.alert(" ERROR how many types 1? 2? 3? or 4?");
+    characterType();
+  }
+    else {
+      return count;
+    }
+    
+    
+
+  }
+
+  
+
+
 // create function for generatePassword()
 
 function generatePassword(){
   // get character length from user  and use the value to demonstate how long it will be 
 var charLength = characterLength();
-return charLength;
-  // get character types from user and demonstrate the types shown
 
+
+
+  // get character types from user and demonstrate the types shown
+  var charType = characterType();
+
+    var types =[];
+
+    for(var i = 0; i < charType; i++){
+       var input = window.prompt("What character types would you like to use? type each one by one  ( 1 = lowercase, 2= uppercase, 3 = numeric, and/or 4 = special characters (TO SELECT AN OPTION CHOOSE THE CORRESPONDING NUMBER )");
+       while(input != 1 && input != 2 && input != 3 &&  input != 4){
+        input = window.prompt(" ERROR Character types only have options 1-4 please try again");
+      }
+      
+         types[i] = input;
+         console.log(types[i]);
+         
+      
+  
+    }
   // finally generate password and return it 
+  
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 

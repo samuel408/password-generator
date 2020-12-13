befb0ced757743e3e66ce6246cbb867e50981267
Samuel <samuel@Samuels-MacBook-Pro.local>
2020-12-12 23:43:17 -0800

    finished main function

--- assets/js/script.js
+++ assets/js/script.js
@@ -19,28 +19,7 @@ function characterLength(){
   and then stores them in an array using a loop.
 
 */
-
-
 function characterType(){
-  
-  
-// function loop(){
-//   var types =[];
-//     for(var i = 0; i < count; i++){
-//        var input = window.prompt("What character types would you like to use ? (lowercase, uppercase, numeric, and/or special characters (SPELL EXACTLY AS THEY APPEAR INCLUDING THE SPACE IN SPECIAL CHARACTER)");
-//        if (input != "lowercase" && input != "uppercase" && input != "numeric" &&  input != "special character"){
-//         window.alert(" ERROR Character types can only be lowercase, uppercase, numeric, and/or special characters");
-//         loop();
-//       }
-//       else {
-//         types[i] = input;
-//       }
-      
-
-//     }
-
-//   }
-
   var count = 0;
   count  = window.prompt("how many character types would you like to use?  please choose between 1-4 types(lowercase, uppercase, numeric, and/or special characters )" );
   if (count < 1 || count  > 4){
@@ -50,75 +29,87 @@ function characterType(){
     else {
       return count;
     }
-    
-    
-
-  }
-
-  
-
-
+}
 // create function for generatePassword()
 
 function generatePassword(){
   // get character length from user  and use the value to demonstate how long it will be 
 var charLength = characterLength();
-
-
-
   // get character types from user and demonstrate the types shown
   var charType = characterType();
+    // var types =[];
+    var upper = false;
+    var lower = false;
+    var numeric = false;
+    var special = false;
 
-    var types =[];
 
     for(var i = 0; i < charType; i++){
        var input = window.prompt("What character types would you like to use? type each one by one  ( 1 = lowercase, 2= uppercase, 3 = numeric, and/or 4 = special characters (TO SELECT AN OPTION CHOOSE THE CORRESPONDING NUMBER )");
        while(input != 1 && input != 2 && input != 3 &&  input != 4){
         input = window.prompt(" ERROR Character types only have options 1-4 please try again");
       }
-      
-         types[i] = input;
-         console.log(types[i]);
-
-      
-  
+       switch ( input ){
+         case '1':
+           lower = true;
+          break;
+        case '2':
+          upper = true;
+          break;
+        case '3':
+          numeric = true;
+          break;
+        case '4' :
+          special = true ;
+       }
+       
     }
-  // finally generate password and return it 
-  // randomly choose how many character types to use 
+
+
+  // finally generate password and return it  
+    
+//character sets
+lowercase = "qwertyuiopasdfghjklzxcvbnm";
+uppercase = "QWERTYUIOPASDFGHJKLZXCVBNM";
+numerical= "1234567890";
+specialChar = "! @#$%^&*";
+all =  "qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM1234567890! @#$%^&*" ;
+//return value variable
+retVal = "";
+//personal set that matches the users selcted criteria
+personalSet = "";
+if (lower === true){
+  personalSet += lowercase;
+}
+if (upper === true){
+  personalSet += uppercase;
+}
+if(numeric === true){
+  personalSet += numerical;
+}
+if (special){
+  personalSet += specialChar;
+}
+console.log (personalSet);
+
+
+for ( var i =  0 ; i < charLength; i++){
+  
+    retVal += personalSet.charAt(Math.floor(Math.random() * personalSet.length));
+
   
 
   
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+return retVal;
+}
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
-
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
   passwordText.value = password;
-
 }
-
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
